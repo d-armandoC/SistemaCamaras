5b32b20c4a017659bdf60ed7e91d5b8570e58bb7
fabricio <fabricio@frsisalimao>
2014-10-09 00:37:49 -0500

    mensaje

--- src/ip_camara_opencv/Visor.java
+++ src/ip_camara_opencv/Visor.java
@@ -47,7 +47,6 @@ public class Visor extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-  
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
